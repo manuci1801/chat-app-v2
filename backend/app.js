9838e66623a29d60eed15b70087f6556f3b3a4ef
Manuci Dev <manuci1801@gmail.com>
2020-12-12 15:27:48 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ mongooseConfig.connectDB();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
 
