448bac780656ebcc925d2f3c2e27283ff1c7cef7
manuci <manuci1801@gmail.com>
2020-12-12 12:56:11 +0700

    update mongodb uri

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -6,7 +6,8 @@ module.exports = {
   EMAIL_TYPE: {
     VERIFY_USER: "VERIFY_USER",
   },
-  MONGO_URI: "mongodb://localhost:27017/mern_boilerplate",
+  MONGO_URI:
+    process.env.MONGO_URI || "mongodb://localhost:27017/mern_boilerplate",
   FRONTEND_URI: "http://localhost:3000",
   SECRET_KEY_JWT: "some thing secret",
   VERIFY_USER_PREFIX: "VERIFY_USER_PREFIX",
