99ec4a0f434e7735a0b081729a1c9ef6a1f2ca79
manhtv <manhtv@rikkeisoft.com>
2021-01-19 13:32:12 +0700

    update design

--- frontend/src/containers/home/ChatContainer.js
+++ frontend/src/containers/home/ChatContainer.js
@@ -11,21 +11,28 @@ import {
   InputGroup,
   InputLeftAddon,
   InputLeftElement,
+  InputRightElement,
+  InputRightAddon,
   Text,
   Tooltip,
+  VStack,
+  Divider,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "../../ColorModeSwitcher";
 import { Icon } from "@chakra-ui/react";
 import {
+  MdCall,
   MdChat,
   MdContacts,
   MdFiberManualRecord,
   MdInfo,
   MdMoreHoriz,
+  MdNotificationsNone,
   MdPhone,
   MdQueue,
   MdSearch,
   MdSettings,
+  MdVideoCall,
   MdVideocam,
 } from "react-icons/md";
 
@@ -42,20 +49,52 @@ const ChatContainer = ({ setIsChat, socket }) => {
         }
         w={{ base: "100%", md: "20em", xl: "20em" }}
         bg="gray.200"
-        px="0.5em"
       >
         {/* <Button onClick={() => setIsRoom(true)}>Mess</Button> */}
-        <Flex align="center" py="0.5em">
-          <Image 
-            boxSize="36px"
-            objectFit="cover" 
-            borderRadius="full" 
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcFLpnetKFyaMFO3SrUmjU4s1Cg94IGGRcjw&usqp=CAU" 
-            alt="Avatar"
-            mr="0.5em"
-          />
-          <Text fontSize="md">Some thing ten</Text>
-        </Flex>
+        <Box px="0.5em">
+          <Flex align="center" py="0.5em">
+            <Image 
+              boxSize="36px"
+              objectFit="cover" 
+              borderRadius="full" 
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcFLpnetKFyaMFO3SrUmjU4s1Cg94IGGRcjw&usqp=CAU" 
+              alt="Avatar"
+              mr="0.5em"
+            />
+            <Text fontSize="md">Some thing ten</Text>
+          </Flex>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<MdSearch color="gray.500" size="1.5em" />}
+            />
+            <Input borderColor="gray.500" variant="outline" type="tel" placeholder="People, groups & messages" />
+            {/* <InputRightAddon 
+              pointerEvents="auto"
+              bg="gray.400"
+              children={<MdVideoCall size="1.5em" />}
+            /> */}
+          </InputGroup>
+          <HStack py="1em" px="0.5em" justify="space-between">
+            <Flex _hover={{ color: "tomato", cursor: "pointer" }}  align="center" justify="center" flexDir="column">
+              <Icon as={MdChat} w="1.5em" h="1.5em" />
+              <Text mt="0" fontSize="xs">Chats</Text>
+            </Flex>
+            <Flex _hover={{ color: "tomato", cursor: "pointer" }}  align="center" justify="center" flexDir="column">
+              <Icon as={MdCall} w="1.5em" h="1.5em" />
+              <Text mt="0" fontSize="xs">Calls</Text>
+            </Flex>
+            <Flex _hover={{ color: "tomato", cursor: "pointer" }}  align="center" justify="center" flexDir="column">
+              <Icon as={MdContacts} w="1.5em" h="1.5em" />
+              <Text mt="0" fontSize="xs">Contacts</Text>
+            </Flex>
+            <Flex _hover={{ color: "tomato", cursor: "pointer" }}  align="center" justify="center" flexDir="column">
+              <Icon as={MdNotificationsNone} w="1.5em" h="1.5em" />
+              <Text mt="0" fontSize="xs">Notifications</Text>
+            </Flex>
+          </HStack>
+        </Box>
+        <Divider orientation="horizontal" borderColor="gray.500" />
       </Box>
       <Box
         display={
